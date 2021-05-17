1a0564298f3c230a065594f69f01e941cfadc545
Jimmy Ritter <j.ritter@f5.com>
2021-05-17 10:55:29 +0100

    linting fixes

--- frontend/src/components/item.single.tsx
+++ frontend/src/components/item.single.tsx
@@ -1,58 +1,65 @@
 import React, { useState } from 'react';
-import { ItemModel, ItemActions } from "../types/item";
+import axios, { AxiosRequestConfig } from 'axios';
+import { ItemModel, ItemActions } from '../types/item';
 import icon from '../assets/icons/delete-white.svg';
-import axios, { AxiosRequestConfig } from "axios";
 
-const Item = ({ _id, name, checked, onCheck, onDelete }: ItemModel & ItemActions) => {
-    const [stateChecked, setStateChecked] = useState(checked);
+const Item = ({
+  _id, name, checked, onCheck, onDelete,
+}: ItemModel & ItemActions) => {
+  const [stateChecked, setStateChecked] = useState(checked);
 
-    const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-    const onChangeCheckbox = (value: boolean) => {
-        setStateChecked(value);
-        onCheck!(_id, value);
+  const onChangeCheckbox = (value: boolean) => {
+    setStateChecked(value);
+    onCheck!(_id, value);
 
-        const item: ItemModel = {
-            _id: _id,
-            name: name,
-            checked: value,
+    const item: ItemModel = {
+      _id,
+      name,
+      checked: value,
+    };
+
+    axios.put(`${baseUrl}/items`, { item })
+      .then((response: AxiosRequestConfig) => {
+        if (!response.data.ok) {
+          onCheck!(_id, !value);
+          setStateChecked(!value);
         }
+      });
+  };
 
-        axios.put(`${baseUrl}/items`, { item })
-            .then((response: AxiosRequestConfig) => {
-                if (!response.data.ok) {
-                    // rollback
-                    onCheck!(_id, !value);
-                    setStateChecked(!value);
-                }
-            });
-    }
+  const onDeleteItem = () => {
+    axios.delete(`${baseUrl}/items/${_id}`)
+      .then((response: AxiosRequestConfig) => {
+        if (response.data.ok) {
+          onDelete!(_id);
+        }
+      });
+  };
 
-    const onDeleteItem = () => {
-        axios.delete(`${baseUrl}/items/${_id}`)
-            .then((response: AxiosRequestConfig) => {
-                if (response.data.ok) {
-                    onDelete!(_id);
-                }
-            });
-    }
-
-    return (
-        <div className={'item-row'}>
-            <input type="checkbox"
-                   defaultChecked={checked}
-                   onChange={(event) => onChangeCheckbox(event.target.checked)}
-            />
-            <span className={stateChecked ? 'checked-item' : ''}>
-                {name}
-            </span>
-            <img src={icon}
-                 alt={'remove item'}
-                 className={'remove-icon'}
-                 onClick={onDeleteItem}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="item-row">
+      <input
+        type="checkbox"
+        defaultChecked={checked}
+        onChange={(event) => onChangeCheckbox(event.target.checked)}
+      />
+      <span className={stateChecked ? 'checked-item' : ''}>
+        {name}
+      </span>
+      <button
+        type="button"
+        onClick={onDeleteItem}
+        className="remove-icon-button"
+      >
+        <img
+          src={icon}
+          alt="remove item"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Item;
--- frontend/src/types/item.tsx
+++ frontend/src/types/item.tsx
@@ -1,10 +1,10 @@
 export declare interface ItemModel {
-    _id: string;
-    name: string;
-    checked: boolean;
+  _id: string;
+  name: string;
+  checked: boolean;
 }
 
 export declare interface ItemActions {
-    onCheck?: (id: string, value: boolean) => void;
-    onDelete?: (id: string) => void;
+  onCheck?: (id: string, value: boolean) => void;
+  onDelete?: (id: string) => void;
 }
